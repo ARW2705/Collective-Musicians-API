058c5471097369d7ee49fd9a7f82e4fe2d5dba26
Andrew Wanex <awanex2705@gmail.com>
2023-01-19 17:35:38 -0800

    feature: add passport json web token auth strategy

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,8 @@
+import createError from 'http-errors'
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
-
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+import jwt from 'jsonwebtoken'
 import User from './models/User'
 
 
@@ -8,11 +10,26 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-const verifyUser = passport.authenticate('UserStrategy', {session: false})
+const jwtStragetyOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.TOKEN_KEY
+}
+passport.use(new JwtStrategy(jwtStragetyOptions, (jwt_payload, done) => {
+  User.findOne({_id: jwt_payload._id}, (error, user) => {
+    if (error) return done(error, false)
+    if (user) return done(null, user)
+    return done(null, false)
+  })
+}))
+
+const getToken = user => jwt.sign(user, process.env.TOKEN_KEY, {expiresIn: '30d'})
+const verifyUser = passport.authenticate('jwt', {session: false})
 const verifyAdmin = (req, res, next) => req.user.admin ? next() : next(createError(403))
 const verifyEditor = (req, res, next) => (req.user.editor || req.user.admin) ? next() : next(createError(403))
 
+
 export {
+  getToken,
   verifyUser,
   verifyAdmin,
   verifyEditor
