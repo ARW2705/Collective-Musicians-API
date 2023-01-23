d7a378ed1a7d5d52dc09816bf3940323363c6592
Andrew Wanex <awanex2705@gmail.com>
2023-01-23 14:21:25 -0800

    refactor: update error handler to send json response for api routes and error page for non-api routes

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import mongoose from 'mongoose'
-
-const API_VERSION = '0.0.1'
-const API_ROUTE = `collective_musicians_${API_VERSION}`
-
 import indexRouter from './routes/index'
-import usersRouter from './routes/users'
+import usersRouter from './routes/users/route'
+import { API_VERSION } from './shared/api-version'
+
 
 const connect = mongoose.connect(
   process.env.MONGO_URL,
@@ -29,35 +27,49 @@ connect.then(
   error => console.error(error)
 )
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const API_ROUTE = `collective_musicians_${API_VERSION}`
+app.use('/', indexRouter)
+app.use(`/${API_ROUTE}/users`, usersRouter)
+
+// catch routes not using the API route
+app.use((req, res, next) => {
+  if (!req.url.includes(API_ROUTE)) {
+    next(createError(400))
+  }
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  if (req.url.includes(API_ROUTE)) {
+    res.status(err.status || 500)
+    res.setHeader('content-type', 'application/json')
+    res.json({ status: err.status, message: err.message })
+  } else {
+    res.status(400)
+    res.render('error')
+  }
+})
 
 export default app
