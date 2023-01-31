56528713226befb07e8ec3c2b005a0e42b49c79b
Andrew Wanex <awanex2705@gmail.com>
2023-01-31 14:23:19 -0800

    refactor: split sheet route into get for non-filtered sheet response and post for filtered response

--- routes/sheets/route.js
+++ routes/sheets/route.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import createError from 'http-errors'
 import { verifyUser } from '../../authenticate'
-import { getSpreadSheetMetaData, getSheetValues, mapColumns, getColumnMap } from '../../sheets-connector/connector'
-import { queryToRange } from './helpers'
+import { getSpreadSheetMetaData, getSheetValues, mapColumns, getColumnMap } from '../../sheets/connector/connector'
+import { queryToRange, getFilteredSheet, getSheet, getColumnNames, getRowLimits } from './helpers'
 
 
 const sheetsRouter = Router()
@@ -10,11 +10,9 @@ const sheetsRouter = Router()
 sheetsRouter.route('/:spreadsheetId')
   .get(verifyUser, async (req, res, next) => {
     try {
-      const response = await getSpreadSheetMetaData(req.params.spreadsheetId)
-      
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
-      res.json(response)
+      res.json(await getSpreadSheetMetaData(req.params.spreadsheetId))
     } catch (error) {
       return next(error)
     }
@@ -25,11 +23,10 @@ sheetsRouter.route('/:spreadsheetId/query')
     try {
       const { sheetName, rowStart, rowEnd, colStart, colEnd } = req.query
       const range = queryToRange(sheetName, rowStart, rowEnd, colStart, colEnd)
-      const response = await getSheetValues(req.params.spreadsheetId, range)
 
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
-      res.json(response)
+      res.json(await getSheetValues(req.params.spreadsheetId, range))
     } catch (error) {
       console.log(error)
       return next(error.name === 'QueryError' ? createError(400, error.message) : error)
@@ -62,11 +59,10 @@ sheetsRouter.route('/:spreadsheetId/refresh-query-terms')
     try {
       const { spreadsheetId } = req.params
       await mapColumns(spreadsheetId)
-      const columnsMap = await getColumnMap(spreadsheetId)
 
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
-      res.json(columnsMap)
+      res.json(await getColumnMap(spreadsheetId))
     } catch (error) {
       return next(error)
     }
@@ -76,15 +72,27 @@ sheetsRouter.route('/:spreadsheetId/sheet')
   .get(verifyUser, async (req, res, next) => {
     try {
       const { spreadsheetId } = req.params
-      const { sheetName } = req.query
-      const { values } = await getSheetValues(spreadsheetId, sheetName)
+      const { page = 1, limit = 50, sheetName } = req.query
+      const { rowStart, rowEnd } = getRowLimits(page, limit)
+      const columnNames = await getColumnNames(spreadsheetId, sheetName)
 
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
-      res.json({
-        keys: values[0],
-        values: values.slice(1)
-      })
+      res.json(await getSheet(spreadsheetId, sheetName, columnNames, rowStart, rowEnd))
+    } catch (error) {
+      return next(error)
+    }
+  })
+  .post(verifyUser, async (req, res, next) => {
+    try {
+      const { spreadsheetId } = req.params
+      const { page = 1, limit = 50, sheetName } = req.query
+      const { filter } = req.body
+      const { rowStart, rowEnd } = getRowLimits(page, limit)
+
+      res.statusCode = 200
+      res.setHeader('content-type', 'application/json')
+      res.json(await getFilteredSheet(spreadsheetId, sheetName, filter, rowStart, rowEnd))
     } catch (error) {
       return next(error)
     }
