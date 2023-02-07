b56e1747a5618c8d254cb86409b4cf8670b2ce1f
Andrew Wanex <awanex2705@gmail.com>
2023-02-07 11:20:04 -0800

    refactor: remove query term mapping; add column name get route

--- routes/sheets/route.js
+++ routes/sheets/route.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import createError from 'http-errors'
 import { verifyUser } from '../../authenticate.js'
-import { getSpreadSheetMetaData, getSheetValues, mapColumns, getColumnMap } from '../../sheets/connector/connector.js'
-import { queryToRange, getFilteredSheet, getSheet, getColumnNames, getRowLimits } from './helpers.js'
+import { getSpreadSheetMetaData } from '../../sheets/connector/connector.js'
+import { getFilteredSheet, getSheet, getColumnNames, getRowLimits } from './helpers.js'
 
 
 const sheetsRouter = Router()
@@ -19,56 +19,6 @@ sheetsRouter.route('/:spreadsheetId')
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
       res.json(response)
-    } catch (error) {
-      return next(error)
-    }
-  })
-
-sheetsRouter.route('/:spreadsheetId/query')
-  .get(verifyUser, async (req, res, next) => {
-    try {
-      const { sheetName, rowStart, rowEnd, colStart, colEnd } = req.query
-      const range = queryToRange(sheetName, rowStart, rowEnd, colStart, colEnd)
-
-      res.statusCode = 200
-      res.setHeader('content-type', 'application/json')
-      res.json(await getSheetValues(req.params.spreadsheetId, range))
-    } catch (error) {
-      console.log(error)
-      return next(error.name === 'QueryError' ? createError(400, error.message) : error)
-    }
-  })
-
-sheetsRouter.route('/:spreadsheetId/query-terms')
-  .get(verifyUser, async (req, res, next) => {
-    try {
-      const columnsMap = await getColumnMap(req.params.spreadsheetId)
-      let queryTerms = {}
-      for (const queryKey in columnsMap) {
-        let sheetTerms = []
-        for (const sheetKey in columnsMap[queryKey]) {
-          sheetTerms = [...sheetTerms, sheetKey]
-        }
-        queryTerms = { ...queryTerms, [queryKey]: sheetTerms }
-      }
-
-      res.statusCode = 200
-      res.setHeader('content-type', 'application/json')
-      res.json(queryTerms)
-    } catch (error) {
-      return next(error)
-    }
-  })
-
-sheetsRouter.route('/:spreadsheetId/refresh-query-terms')
-  .get(verifyUser, async (req, res, next) => {
-    try {
-      const { spreadsheetId } = req.params
-      await mapColumns(spreadsheetId)
-
-      res.statusCode = 200
-      res.setHeader('content-type', 'application/json')
-      res.json(await getColumnMap(spreadsheetId))
     } catch (error) {
       return next(error)
     }
@@ -99,6 +49,22 @@ sheetsRouter.route('/:spreadsheetId/sheet')
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
       res.json(await getFilteredSheet(spreadsheetId, sheetName, filter, rowStart, rowEnd))
+    } catch (error) {
+      return next(error)
+    }
+  })
+
+sheetsRouter.route('/:spreadsheetId/sheet/columns')
+  .get(async (req, res, next) => {
+    try {
+      const { spreadsheetId } = req.params
+      const { sheetName } = req.query
+      const response = (await getColumnNames(spreadsheetId, sheetName))
+        .filter(name => name !== '')
+
+      res.statusCode = 200
+      res.setHeader('content-type', 'application/json')
+      res.json(response)
     } catch (error) {
       return next(error)
     }
